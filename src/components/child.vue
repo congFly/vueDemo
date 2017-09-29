3c4f2d6f379b0218b6a4609db80f35936b77d889
fcc <fcc@cognichain.com>
2017-09-29 13:58:23 +0800

    多次点击换颜色

--- src/components/child.vue
+++ src/components/child.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="child">
     <ul class="list-inline">
-      <li><a href="#" class="btn btn-primary" @click="changeColor">&nbsp;</a></li>
-      <li><a href=" #" class="btn btn-info" @click="changeColor">&nbsp;</a></li>
-      <li><a href="#" class="btn btn-danger" @click="changeColor">&nbsp;</a></li>
-      <li><a href="#" class="btn btn-warning" @click="changeColor">&nbsp;</a></li>
-      <li><a href="#" class="btn btn-default" @click="changeColor">&nbsp;</a></li>
+      <li><a href="#" class="btn btn-primary" @click="changeColor1">&nbsp;</a></li>
+      <li><a href=" #" class="btn btn-info" @click="changeColor2">&nbsp;</a></li>
+      <li><a href="#" class="btn btn-danger" @click="changeColor3">&nbsp;</a></li>
+      <li><a href="#" class="btn btn-warning" @click="changeColor4">&nbsp;</a></li>
+      <li><a href="#" class="btn btn-default" @click="changeColor5">&nbsp;</a></li>
     </ul>
+    <!--<button class="child_1" @click="tellToParent">tellMyDad</button>-->
+    <input class="input" v-model="childSaySomething" @keyup.enter="tellToParent">
+    <br>
+    <br>
     <button @click="getParent">getParentMsg</button>
   </div>
 </template>
@@ -22,20 +26,39 @@
 </style>
 <script>
   export default{
-    /*props: ['activeColor', 'activeColor1','parentMsg'],*/
-    props: ['parentMsg'],
+    props: [/*'activeColor', 'activeColor1',*/'parentMsg'],
     data(){
       return {
-        childColor: 'yellow',
+        childColor1: '#337ab7',
+        childColor2: '#5bc0de',
+        childColor3: '#d9534f',
+        childColor4: '#f0ad4e',
+        childColor5: '#ddd',
+        childSaySomething: ''
       }
     },
     methods: {
-      changeColor: function () {
-        console.log(this.childColor);
-        this.$emit('change', this.childColor);
+      changeColor1: function () {
+        this.$emit('change', this.childColor1);
+      },
+      changeColor2: function () {
+        this.$emit('change', this.childColor2);
+      },
+      changeColor3: function () {
+        this.$emit('change', this.childColor3);
+      },
+      changeColor4: function () {
+        this.$emit('change', this.childColor4);
+      },
+      changeColor5: function () {
+        this.$emit('change', this.childColor5);
       },
       getParent: function () {
         console.log(this.parentMsg)
+      },
+      tellToParent: function (childSaySomething) {
+          console.log('fcc')
+        this.$emit('listenChild', childSaySomething);
       }
     }
   }
--- src/components/parent.vue
+++ src/components/parent.vue
@@ -3,22 +3,23 @@
     <br/>
   <!--  <p v-model="parentMsg">{{parentMsg}}</p>-->
     <input v-model="parentMsg">
+    <p >I want to hear what you say:</p>
     <ul class="list-inline">
       <li>对应第一个child</li>
-      <li @click="changeChild1Color"><a href="#" class="btn btn-primary">&nbsp;</a>
+      <li @click="changeChild1Color1"><a href="#" class="btn btn-primary">&nbsp;</a>
       </li>
-      <li @click="changeChild1Color"><a href="#" class="btn btn-info">&nbsp;</a></li>
-      <li @click="changeChild1Color"><a href="#" class="btn btn-danger">&nbsp;</a></li>
-      <li @click="changeChild1Color"><a href="#" class="btn btn-warning">&nbsp;</a></li>
-      <li @click="changeChild1Color"><a href="#" class="btn btn-default">&nbsp;</a></li>
+      <li @click="changeChild1Color2"><a href="#" class="btn btn-info">&nbsp;</a></li>
+      <li @click="changeChild1Color3"><a href="#" class="btn btn-danger">&nbsp;</a></li>
+      <li @click="changeChild1Color4"><a href="#" class="btn btn-warning">&nbsp;</a></li>
+      <li @click="changeChild1Color5"><a href="#" class="btn btn-default">&nbsp;</a></li>
     </ul>
     <ul class="list-inline">
-      <li @click="changeChild2Color">对应第二个child</li>
-      <li @click="changeChild2Color"><a href="#" class="btn btn-primary">&nbsp;</a></li>
-      <li @click="changeChild2Color"><a href="#" class="btn btn-info">&nbsp;</a></li>
-      <li @click="changeChild2Color"><a href="#" class="btn btn-danger">&nbsp;</a></li>
-      <li @click="changeChild2Color"><a href="#" class="btn btn-warning">&nbsp;</a></li>
-      <li @click="changeChild2Color"><a href="#" class="btn btn-default">&nbsp;</a></li>
+      <li>对应第二个child</li>
+      <li @click="changeChild2Color1"><a href="#" class="btn btn-primary">&nbsp;</a></li>
+      <li @click="changeChild2Color2"><a href="#" class="btn btn-info">&nbsp;</a></li>
+      <li @click="changeChild2Color3"><a href="#" class="btn btn-danger">&nbsp;</a></li>
+      <li @click="changeChild2Color4"><a href="#" class="btn btn-warning">&nbsp;</a></li>
+      <li @click="changeChild2Color5"><a href="#" class="btn btn-default">&nbsp;</a></li>
     </ul>
     <hr/>
     <child class="child1" :style="{background:activeColor}" v-on:change="changeParentColor" :parentMsg="parentMsg"></child>
@@ -44,11 +45,37 @@
       }
     },
     methods: {
-      changeChild1Color(){
-        this.activeColor = 'green';
+      changeChild1Color1(){
+        this.activeColor = '#337ab7';
       },
-      changeChild2Color(){
-        this.activeColor1 = 'grey'
+      changeChild1Color2(){
+        this.activeColor = '#5bc0de';
+      },
+      changeChild1Color3(){
+        this.activeColor = '#d9534f';
+      },
+      changeChild1Color4(){
+        this.activeColor = '#f0ad4e';
+      },
+      changeChild1Color5(){
+        this.activeColor = '#ddd';
+      },
+
+
+      changeChild2Color1(){
+        this.activeColor1 = '#337ab7';
+      },
+      changeChild2Color2(){
+        this.activeColor1 = '#5bc0de';
+      },
+      changeChild2Color3(){
+        this.activeColor1 = '#d9534f';
+      },
+      changeChild2Color4(){
+        this.activeColor1 = '#f0ad4e';
+      },
+      changeChild2Color5(){
+        this.activeColor1 = '#ddd';
       },
       changeParentColor(childColor){
         this.parentColor = childColor;
