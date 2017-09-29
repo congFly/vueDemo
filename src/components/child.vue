74dafa7b1b1cd1e896acb43e19e27895465fe966
fcc <fcc@cognichain.com>
2017-09-29 10:57:46 +0800

    vue

---
+++ src/components/child.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="child">
+    <ul class="list-inline">
+      <li><a href="#" class="btn btn-primary" @click="changeColor">&nbsp;</a></li>
+      <li><a href=" #" class="btn btn-info" @click="changeColor">&nbsp;</a></li>
+      <li><a href="#" class="btn btn-danger" @click="changeColor">&nbsp;</a></li>
+      <li><a href="#" class="btn btn-warning" @click="changeColor">&nbsp;</a></li>
+      <li><a href="#" class="btn btn-default" @click="changeColor">&nbsp;</a></li>
+    </ul>
+    <button @click="getParent">getParentMsg</button>
+  </div>
+</template>
+<style>
+  .child {
+    float: left;
+    margin-right: 40px;
+    border-color: black;
+    border: 1px solid;
+    height: 200px;
+    width: 400px;
+  }
+</style>
+<script>
+  export default{
+    /*props: ['activeColor', 'activeColor1','parentMsg'],*/
+    props: ['parentMsg'],
+    data(){
+      return {
+        childColor: 'yellow',
+      }
+    },
+    methods: {
+      changeColor: function () {
+        console.log(this.childColor);
+        this.$emit('change', this.childColor);
+      },
+      getParent: function () {
+        console.log(this.parentMsg)
+      }
+    }
+  }
+</script>
---
+++ src/components/parent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="col-xs-10 col-xs-offset-1 parent" :style="{background:parentColor}">
+    <br/>
+  <!--  <p v-model="parentMsg">{{parentMsg}}</p>-->
+    <input v-model="parentMsg">
+    <ul class="list-inline">
+      <li>对应第一个child</li>
+      <li @click="changeChild1Color"><a href="#" class="btn btn-primary">&nbsp;</a>
+      </li>
+      <li @click="changeChild1Color"><a href="#" class="btn btn-info">&nbsp;</a></li>
+      <li @click="changeChild1Color"><a href="#" class="btn btn-danger">&nbsp;</a></li>
+      <li @click="changeChild1Color"><a href="#" class="btn btn-warning">&nbsp;</a></li>
+      <li @click="changeChild1Color"><a href="#" class="btn btn-default">&nbsp;</a></li>
+    </ul>
+    <ul class="list-inline">
+      <li @click="changeChild2Color">对应第二个child</li>
+      <li @click="changeChild2Color"><a href="#" class="btn btn-primary">&nbsp;</a></li>
+      <li @click="changeChild2Color"><a href="#" class="btn btn-info">&nbsp;</a></li>
+      <li @click="changeChild2Color"><a href="#" class="btn btn-danger">&nbsp;</a></li>
+      <li @click="changeChild2Color"><a href="#" class="btn btn-warning">&nbsp;</a></li>
+      <li @click="changeChild2Color"><a href="#" class="btn btn-default">&nbsp;</a></li>
+    </ul>
+    <hr/>
+    <child class="child1" :style="{background:activeColor}" v-on:change="changeParentColor" :parentMsg="parentMsg"></child>
+    <child class="child2" :style="{background:activeColor1}" v-on:change="changeParentColor"></child>
+  </div>
+</template>
+<style>
+  .parent {
+    height: 400px;
+    border: 3px solid;
+  }
+</style>
+<script>
+  import child from './child';
+
+  export default{
+    data(){
+      return {
+        parentMsg: 'i am  parent',
+        activeColor: '',
+        activeColor1: '',
+        parentColor: '',
+      }
+    },
+    methods: {
+      changeChild1Color(){
+        this.activeColor = 'green';
+      },
+      changeChild2Color(){
+        this.activeColor1 = 'grey'
+      },
+      changeParentColor(childColor){
+        this.parentColor = childColor;
+      }
+    },
+    components: {
+      child
+    }
+  }
+
+</script>
